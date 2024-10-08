80c4da029d9b0043529e821a60ce104cd75a248a
stasel <2033301+stasel@users.noreply.github.com>
2024-10-08 13:56:46 +0200

    More fixes

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -6,6 +6,7 @@ const Home = () => {
   const headerStyle = {
     background: "aqua",
   };
+
   return (
     <div data-testid={TEST_ID.container}>
       <h1 style={headerStyle}>This is the homepage</h1>
