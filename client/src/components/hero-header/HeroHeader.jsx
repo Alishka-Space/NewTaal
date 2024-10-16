ef8ff1d527af7bd5237df7c12c1bc386ff0d1c78
Ali Ibrahim <165920255+Alishka-Space@users.noreply.github.com>
2024-10-16 23:16:56 +0200

    add styling landing page

--- client/src/components/hero-header/HeroHeader.jsx
+++ client/src/components/hero-header/HeroHeader.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "../hero-header/hero.css";
 import { Link } from "react-router-dom";
+import hero from "../../../public/photo/hero-chang.jpg";
 
 const HeroHeader = () => {
   return (
     <div className="hero-header">
+      <img src={hero} alt="hero-img" className="hero-img" />
       <Link className="hero-btn">Sign Up Now</Link>
     </div>
   );
