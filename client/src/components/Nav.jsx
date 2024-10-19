1075a0df01ad896f06265ab8a53c35f893c6801d
Moayad Mohammed <moayad.ashabi@gmail.com>
2024-10-20 01:15:14 +0200

    edited navbar based on logged user

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -3,17 +3,24 @@ import { Link } from "react-router-dom";
 import TallLogo from "../images/logo-1.png";
 import { useState } from "react";
 import "./nav.css";
+import { AuthContext } from "../context/AuthContext";
 
 const Nav = () => {
   const [toggle, setToggle] = useState(false);
 
+  const { authState } = React.useContext(AuthContext);
+
   return (
     <header className="header">
       <div className="logo">
-        <img src={TallLogo} alt="logo" className="logo-img" />
+        <Link to="/" className="navbar-link">
+          <img src={TallLogo} alt="logo" className="logo-img" />
+        </Link>
         <div className="logo-text">
-          <b>Taal</b>
-          <b>Coach</b>
+          <Link to="/" className="navbar-link">
+            <b>Taal</b>
+            <b>Coach</b>
+          </Link>
         </div>
       </div>
       <nav
@@ -23,38 +30,80 @@ const Nav = () => {
         className="navbar"
       >
         <ul className="navbar-links">
-          <Link to="/" className="navbar-link">
-            <i className="bi bi-house-fill"></i>
-            Home
-          </Link>
-          <Link to="/user" className="navbar-link">
-            <i className="bi bi-person-plus-fill"></i>
-            Users
-          </Link>
-          <Link to="/about" className="navbar-link">
-            <i className="bi bi-bank2"></i>
-            About us
-          </Link>
-          <Link className="navbar-link">
-            <i className="bi bi-person-plus-fill"></i>
-            Coaches
-          </Link>
-          <Link to="/signin" className="navbar-link">
-            <i className="bi bi-arrow-right-square-fill"></i>
-            Log-in
-          </Link>
-          <Link to="/signup" className="navbar-link">
-            <i className="bi bi-arrow-right-square-fill"></i>
-            sign-up
-          </Link>
-          <Link to="/coachProfile" className="navbar-link">
-            <i className="bi bi-arrow-right-square-fill"></i>
-            Coach Profile
-          </Link>
-          <Link to="/learnerProfile" className="navbar-link">
-            <i className="bi bi-arrow-right-square-fill"></i>
-            Learner Profile
-          </Link>
+          {authState.role === "learner" ? (
+            <>
+              <Link to="/userhome" className="navbar-link">
+                <i className="bi bi-house-fill"></i>
+                Home
+              </Link>
+              <Link to="/user" className="navbar-link">
+                <i className="bi bi-person-plus-fill"></i>
+                Users
+              </Link>
+              <Link to="/about" className="navbar-link">
+                <i className="bi bi-bank2"></i>
+                About us
+              </Link>
+              <Link
+                to={`/userProfile/:${authState.id}`}
+                className="navbar-link"
+              >
+                <i className="bi bi-arrow-right-square-fill"></i>
+                Profile
+              </Link>
+            </>
+          ) : authState.role === "coach" ? (
+            <>
+              <Link to={"/coachProfile"} className="navbar-link">
+                <i className="bi bi-house-fill"></i>
+                Home
+              </Link>
+              <Link to="/user" className="navbar-link">
+                <i className="bi bi-person-plus-fill"></i>
+                Users
+              </Link>
+              <Link to="/about" className="navbar-link">
+                <i className="bi bi-bank2"></i>
+                About us
+              </Link>
+              <Link to="/coachProfile" className="navbar-link">
+                <i className="bi bi-arrow-right-square-fill"></i>
+                Profile
+              </Link>
+            </>
+          ) : (
+            <>
+              <Link to="/" className="navbar-link">
+                <i className="bi bi-house-fill"></i>
+                Home
+              </Link>
+              <Link to="/user" className="navbar-link">
+                <i className="bi bi-person-plus-fill"></i>
+                Users
+              </Link>
+              <Link to="/about" className="navbar-link">
+                <i className="bi bi-bank2"></i>
+                About us
+              </Link>
+            </>
+          )}
+          {!authState.token ? (
+            <>
+              <Link to="/signin" className="navbar-link">
+                <i className="bi bi-arrow-right-square-fill"></i>
+                Log-in
+              </Link>
+              <Link to="/signup" className="navbar-link">
+                <i className="bi bi-arrow-right-square-fill"></i>
+                sign-up
+              </Link>
+            </>
+          ) : (
+            <Link to="/signout" className="navbar-link">
+              <i className="bi bi-arrow-right-square-fill"></i>
+              Log-out
+            </Link>
+          )}
         </ul>
       </nav>
 
--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -8,32 +8,36 @@ export const AuthProvider = ({ children }) => {
     token: localStorage.getItem("token"),
     user: localStorage.getItem("user"),
     role: localStorage.getItem("role"),
+    id: localStorage.getItem("id"),
   });
 
   useEffect(() => {
     const token = localStorage.getItem("token");
     const user = localStorage.getItem("user");
     const role = localStorage.getItem("role");
+    const id = localStorage.getItem("id");
 
     if (token && user && role) {
-      setAuthState({ token, user, role });
+      setAuthState({ token, user, role, id });
     }
   }, []);
 
-  const login = (token, user, role) => {
+  const login = (token, user, role, id) => {
     localStorage.setItem("token", token);
     localStorage.setItem("user", user);
     localStorage.setItem("role", role);
+    localStorage.setItem("id", id);
 
-    setAuthState({ token, user, role });
+    setAuthState({ token, user, role, id });
   };
 
   const logout = () => {
     localStorage.removeItem("token");
     localStorage.removeItem("user");
     localStorage.removeItem("role");
+    localStorage.removeItem("id");
 
-    setAuthState({ token: null, user: null, role: null });
+    setAuthState({ token: null, user: null, role: null, id: null });
   };
 
   return (
