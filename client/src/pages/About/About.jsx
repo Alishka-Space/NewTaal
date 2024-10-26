2737766e33e99e7df99a2df106e18adebd6633df
Ali Ibrahim <165920255+Alishka-Space@users.noreply.github.com>
2024-10-27 00:09:02 +0200

    run autofix

--- client/src/pages/About/About.jsx
+++ client/src/pages/About/About.jsx
@@ -6,9 +6,8 @@ const About = () => {
     <div className="bout">
       <section className="about-section">
         <div className="about-content">
-          <h1 className="about-title">About TaalCoach</h1>
           <p className="about-text">
-            For Coaches Coaches at TaalCoach Our dedicated coaches are
+            <b>For Coaches</b> Coaches at TaalCoach Our dedicated coaches are
             experienced professionals, each bringing a unique approach to
             language learning. Whether you are aiming to master conversational
             language, prepare for exams, or improve your business communication,
@@ -20,16 +19,16 @@ const About = () => {
             easily fit into your life.
           </p>
           <p className="about-text">
-            For Trainees Trainees at TaalCoach Our trainees are passionate
-            individuals eager to improve their language skills, whether for
-            travel, work, cultural exploration, or personal growth. TaalCoach
-            offers a seamless way for learners to connect with coaches who align
-            with their learning goals and language levels. Each trainee can
-            browse available coaches, review profiles, and select a language
-            coach based on proficiency and teaching style. The platform`s
-            flexible scheduling,TaalCoach empowers learners to reach their
-            language potential through 1-on-1 sessions designed to be as unique
-            as each individual.
+            <b>For Trainees </b>Trainees at TaalCoach Our trainees are
+            passionate individuals eager to improve their language skills,
+            whether for travel, work, cultural exploration, or personal growth.
+            TaalCoach offers a seamless way for learners to connect with coaches
+            who align with their learning goals and language levels. Each
+            trainee can browse available coaches, review profiles, and select a
+            language coach based on proficiency and teaching style. The
+            platform`s flexible scheduling,TaalCoach empowers learners to reach
+            their language potential through 1-on-1 sessions designed to be as
+            unique as each individual.
           </p>
         </div>
       </section>
