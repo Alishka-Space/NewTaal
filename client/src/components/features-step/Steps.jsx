e2601c9bf76088334825ac02500c92ebfa3847a2
Ali Ibrahim <165920255+Alishka-Space@users.noreply.github.com>
2024-10-27 23:55:57 +0100

    styling landing pag steps

--- client/src/components/features-step/Steps.jsx
+++ client/src/components/features-step/Steps.jsx
@@ -1,23 +1,25 @@
 import React from "react";
 import "./steps.css";
-import stepOne from "../../../public/steps/step1.png";
-import steptree from "../../../public/steps/step3.png";
-import stepFour from "../../../public/steps/step4.png";
+
 const Steps = () => {
   return (
     <div className="features-steps">
       <div className="steps">
-        <div className="step">
-          <img src={stepOne} alt="step-img" className="step-img" />
-          <span>Connect with a mentor</span>
+        <div className="step-one">
+          <span>Create Your Account</span>
+          <p>Sign up to get started with your personalized learning journey.</p>
         </div>
-        <div className="step">
-          <img src={steptree} alt="step-img" className="step-img" />
-          <span>Book your session</span>
+        <div className="step-two">
+          <span>Connect with a Mentor</span>
+          <p>Find a mentor that aligns with your goals and preferences.</p>
         </div>
-        <div className="step">
+        <div className="step-three">
+          <span>Book Your Session</span>
+          <p>Schedule a time that works best for you to begin learning.</p>
+        </div>
+        <div className="step-four">
           <span>Start Learning</span>
-          <img src={stepFour} alt="step-img" className="step-img" />
+          <p>Engage in interactive lessons tailored just for you.</p>
         </div>
       </div>
     </div>
