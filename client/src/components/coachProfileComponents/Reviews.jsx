2770af42a317dfef677e9a597bedb90b05cc8009
OzlemKaraboga <ozlemkaraboga33@gmail.com>
2024-11-02 00:51:15 +0100

    Added pagination to the coachprofile page and styling

--- client/src/components/coachProfileComponents/Reviews.jsx
+++ client/src/components/coachProfileComponents/Reviews.jsx
@@ -10,11 +10,15 @@ import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
+import TablePagination from "@mui/material/TablePagination";
+
 import useFetch from "../../hooks/useFetch";
 
 const Reviews = () => {
   const { id } = useParams();
   const [reviewsData, setReviewsData] = useState();
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
 
   const { performFetch, cancelFetch } = useFetch(
     `/review/coach/${id}`,
@@ -30,6 +34,15 @@ const Reviews = () => {
     });
     return cancelFetch;
   }, []);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
 
   return (
     <Grid container>
@@ -49,35 +62,61 @@ const Reviews = () => {
         <Card sx={{ p: 1, borderRadius: "10px", bgcolor: "#f0f0f0", my: 2 }}>
           <Typography fontWeight="bold">Reviews</Typography>
         </Card>
+
         <Box>
           <TableContainer component={Paper}>
             <Table sx={{ minWidth: 650 }} aria-label="simple table">
               <TableHead>
                 <TableRow>
-                  <TableCell></TableCell>
-                  <TableCell>Learner Name</TableCell>
-                  <TableCell>Rating</TableCell>
-                  <TableCell>Comments</TableCell>
+                  <TableCell sx={{ fontWeight: "bold", width: 180 }}>
+                    Learner Name
+                  </TableCell>
+                  <TableCell sx={{ fontWeight: "bold", width: 4 }}>
+                    Rating
+                  </TableCell>
+                  <TableCell sx={{ fontWeight: "bold" }}>Comments</TableCell>
                 </TableRow>
               </TableHead>
               <TableBody>
                 {reviewsData &&
-                  reviewsData.map((row, index) => (
-                    <TableRow
-                      key={index}
-                      sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                    >
-                      <TableCell component="th" scope="row">
-                        {row.name}
-                      </TableCell>
-                      <TableCell>{row.learner_name}</TableCell>
-                      <TableCell>{row.rating}</TableCell>
-                      <TableCell>{row.comments}</TableCell>
-                    </TableRow>
-                  ))}
+                  reviewsData
+                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                    .map((row, index) => (
+                      <TableRow
+                        key={index}
+                        sx={{
+                          "&:last-child td, &:last-child th": { border: 0 },
+                        }}
+                      >
+                        <TableCell sx={{ bgcolor: "#e1bee7" }}>
+                          {row.learner_name}
+                        </TableCell>
+                        <TableCell
+                          sx={{
+                            textAlign: "center",
+                            bgcolor: "#e6e6fa",
+                            fontWeight: "bold",
+                          }}
+                        >
+                          {row.rating}
+                        </TableCell>
+                        <TableCell sx={{ bgcolor: "#e6e6fa" }}>
+                          {row.comments}
+                        </TableCell>
+                      </TableRow>
+                    ))}
               </TableBody>
             </Table>
           </TableContainer>
+          <TablePagination
+            rowsPerPageOptions={[5, 10, 25]}
+            component="div"
+            count={reviewsData ? reviewsData.length : 0}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+          />
         </Box>
       </Paper>
     </Grid>
