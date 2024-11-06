482d4d94e9da932329c7b0239a4b2e40b5bf113c
Ali Ibrahim <165920255+Alishka-Space@users.noreply.github.com>
2024-11-06 19:29:36 +0100

    styling featers and set responsive page

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-// import Banner from "../../components/banner/Banner";
+import Banner from "../../components/banner/Banner";
 import Features from "../../components/features/Features";
 import HeroHeader from "../../components/hero-header/HeroHeader";
-// import Steps from "../../components/features-step/Steps";
+import Steps from "../../components/features-step/Steps";
 
 import TEST_ID from "./Home.testid";
 
@@ -11,8 +11,8 @@ const Home = () => {
     <div data-testid={TEST_ID.container}>
       <HeroHeader />
       <Features />
-      {/* <Banner />
-      <Steps /> */}
+      <Banner />
+      <Steps />
     </div>
   );
 };
