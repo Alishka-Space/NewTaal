04823049ec32410cc1597109e4525bd17e6b5d4f
Moayad Mohammed <moayad.ashabi@gmail.com>
2024-11-10 10:20:34 +0100

    created availability feature backend

--- server/src/controllers/availability.js
+++ server/src/controllers/availability.js
@@ -1,6 +1,7 @@
 import Availability, { validateAvailability } from "../models/Availability.js";
 import { logError } from "../util/logging.js";
 import validationErrorMessage from "../util/validationErrorMessage.js";
+import Coach from "../models/Coach.js";
 
 export const getAvailability = async (req, res) => {
   try {
@@ -17,18 +18,38 @@ export const getAvailability = async (req, res) => {
 
 export const createAvailability = async (req, res) => {
   try {
-    const { availability } = req.body;
+    const { daysOfWeek, timeSlots, toggleAvailability } = req.body;
+    const { id } = req.params;
 
-    if (typeof availability !== "object") {
-      res.status(400).json({
-        success: false,
-        msg: `You need to provide a 'availability' object. Received: ${JSON.stringify(
-          availability,
-        )}`,
-      });
+    if (daysOfWeek == null) {
+      res.status(400).json({ success: false, msg: "daysOfWeek is required" });
+    }
 
+    if (timeSlots == null) {
+      res.status(400).json({ success: false, msg: "timeSlots is required" });
+    }
+
+    if (toggleAvailability == null) {
+      res
+        .status(400)
+        .json({ success: false, msg: "toggleAvailability is required" });
+    }
+
+    const coach = await Coach.findOne({ user_id: id });
+
+    if (!coach) {
+      res
+        .status(404)
+        .json({ success: false, msg: `Coach with id ${id} not found` });
       return;
     }
+
+    const availability = {
+      coach_id: id,
+      daysOfWeek,
+      timeSlots,
+      toggleAvailability,
+    };
 
     const errorList = validateAvailability(availability);
 
@@ -46,6 +67,20 @@ export const createAvailability = async (req, res) => {
     res.status(500).json({
       success: false,
       msg: "Unable to create availability, try again later",
+    });
+  }
+};
+
+export const getAvailabilityByCoachId = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const availability = await Availability.find({ coach_id: id });
+    res.status(200).json({ success: true, result: availability });
+  } catch (error) {
+    logError(error);
+    res.status(500).json({
+      success: false,
+      msg: "Unable to get availability, try again later",
     });
   }
 };
--- server/src/routes/availability.js
+++ server/src/routes/availability.js
@@ -2,11 +2,13 @@ import express from "express";
 import {
   createAvailability,
   getAvailability,
+  getAvailabilityByCoachId,
 } from "../controllers/availability.js";
 
 const availabilityRouter = express.Router();
 
 availabilityRouter.get("/", getAvailability);
-availabilityRouter.post("/create", createAvailability);
+availabilityRouter.post("/create/:id", createAvailability);
+availabilityRouter.get("/coach/:id", getAvailabilityByCoachId);
 
 export default availabilityRouter;
