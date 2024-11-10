a36ea9276aaeb31b87e582a0b3041470cd2e8b7b
Moayad Mohammed <moayad.ashabi@gmail.com>
2024-11-10 13:05:07 +0100

    Updated on user create to add availability

--- server/src/controllers/availability.js
+++ server/src/controllers/availability.js
@@ -1,7 +1,6 @@
 import Availability, { validateAvailability } from "../models/Availability.js";
 import { logError } from "../util/logging.js";
 import validationErrorMessage from "../util/validationErrorMessage.js";
-import Coach from "../models/Coach.js";
 
 export const getAvailability = async (req, res) => {
   try {
@@ -16,52 +15,16 @@ export const getAvailability = async (req, res) => {
   }
 };
 
-export const createAvailability = async (req, res) => {
+export const createAvailability = async (user, req, res) => {
   try {
-    const { daysOfWeek, timeSlots, toggleAvailability } = req.body;
-    const { id } = req.params;
-
-    if (daysOfWeek == null) {
-      res.status(400).json({ success: false, msg: "daysOfWeek is required" });
-    }
-
-    if (timeSlots == null) {
-      res.status(400).json({ success: false, msg: "timeSlots is required" });
-    }
-
-    if (toggleAvailability == null) {
-      res
-        .status(400)
-        .json({ success: false, msg: "toggleAvailability is required" });
-    }
-
-    const coach = await Coach.findOne({ user_id: id });
-
-    if (!coach) {
-      res
-        .status(404)
-        .json({ success: false, msg: `Coach with id ${id} not found` });
-      return;
-    }
-
     const availability = {
-      coach_id: id,
-      daysOfWeek,
-      timeSlots,
-      toggleAvailability,
+      coach_id: user._id,
+      daysOfWeek: [],
+      timeSlots: [],
+      toggleAvailability: false,
     };
 
-    const errorList = validateAvailability(availability);
-
-    if (errorList.length > 0) {
-      res
-        .status(400)
-        .json({ success: false, msg: validationErrorMessage(errorList) });
-    } else {
-      const newAvailability = await Availability.create(availability);
-
-      res.status(201).json({ success: true, availability: newAvailability });
-    }
+    await Availability.create(availability);
   } catch (error) {
     logError(error);
     res.status(500).json({
