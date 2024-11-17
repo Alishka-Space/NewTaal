7a5d5ebd562b8f4df34a1d8053723fcc096c7a4d
Ali Ibrahim <165920255+Alishka-Space@users.noreply.github.com>
2024-11-17 13:44:08 +0100

    coach info set responsive width

--- client/src/components/coachProfileComponents/PersonalInfoCoach.jsx
+++ client/src/components/coachProfileComponents/PersonalInfoCoach.jsx
@@ -59,7 +59,7 @@ const PersonalInfoCoach = (props) => {
           p: 2,
           mt: 4,
           mb: 1,
-          width: { xs: "100%", sm: "80%", md: 700 },
+          width: { xs: "100%", sm: "80%", md: 750 },
           height: 470,
         }}
         variant="elevation"
@@ -74,7 +74,7 @@ const PersonalInfoCoach = (props) => {
             <Grid>
               <Box
                 sx={{
-                  width: { xs: "80%", sm: "90%", md: 110 },
+                  width: { xs: "90%", sm: "90%", md: 110 },
                 }}
               >
                 <Stack spacing={2}>
@@ -103,7 +103,7 @@ const PersonalInfoCoach = (props) => {
                   <Item
                     sx={{
                       height: 45,
-                      width: { xs: "100%", sm: "90%", md: 450 },
+                      width: { xs: "100%", sm: "100%", md: 450 },
                     }}
                   >
                     {" "}
@@ -131,7 +131,7 @@ const PersonalInfoCoach = (props) => {
                   <Item
                     sx={{
                       height: 45,
-                      width: { xs: "100%", sm: "90%", md: 450 },
+                      width: { xs: "100%", sm: "100%", md: 450 },
                     }}
                   >
                     {" "}
@@ -157,7 +157,7 @@ const PersonalInfoCoach = (props) => {
                   <Item
                     sx={{
                       height: 45,
-                      width: { xs: "100%", sm: "90%", md: 450 },
+                      width: { xs: "100%", sm: "100%", md: 450 },
                     }}
                   >
                     {!isEdit && (
